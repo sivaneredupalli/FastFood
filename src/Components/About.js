cb634641e1b98903aab32ecaebf90cc2c29144eb
sivaneredupalli <neredupallisiva3@gmail.com>
2025-05-01 21:47:02 +0530

    Updated About Component

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,15 +3,15 @@ import UserClass from "./UserClass";
 const About = ()=>{
     return (
         <div className="text-center m-4 p-4">
-        <h1>Namaste React Webseries</h1>
-         <h1>Teaches By Akshay Saini</h1>
-         <div className="aboutus">
+        <h1 className="text-xl font-semibold">Class Component Demonstration </h1>
+         
+         <div className=" m-4 p-4">
          <User name="Siva Neredupalli(Functional)"/>
          </div>
-         <div className="aboutus">
+         <div className=" m-4 p-4">
          <UserClass name="First Child(Class)"/>
          </div>
-         <div className="aboutus">
+         <div className=" m-4 p-4">
          <UserClass name="Second Child(Class)"/>
          </div>
          </div>   

aea55cb2d9f8cf4fce85cfbd740d64f900615c45
sivaneredupalli <neredupallisiva3@gmail.com>
2025-05-01 19:26:57 +0530

    Styling Components

--- src/Components/RestaurantCategoryItems.js
+++ src/Components/RestaurantCategoryItems.js
@@ -11,11 +11,11 @@ const RestaurantCategoryItems = ({data,showItemIndex,setShowItemIndex})=>{
         <div className="w-6/12 bg-gray-100 p-4 mx-auto my-4 rounded-lg">
         <div className="flex justify-between cursor-pointer" onClick={handleItems}>
         <span className="font-bold text-lg">{data?.title} ({data?.itemCards?.length})</span>
-        <span>{showItemIndex ? "🔼" : "🔽"}</span>
+        <span>{showItemIndex ? "🔽" : "🔼"}</span>
         {/* //Accordion body */}
         </div>
         {showItemIndex && <ItemsList Items={data?.itemCards}/>}
-        </div>
+        </div> 
     );
 };
 export default RestaurantCategoryItems;
--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0NDG_SR77n8xtaRwNksZyk1oFmSQiQKBN8X4OQj68dwNc8hjWh3QI3qA&s";
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId="
-export const RESTAURANTS_URL= "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const RESTAURANTS_URL= "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4364048&lng=78.4492613&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
